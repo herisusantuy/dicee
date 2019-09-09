62a4e02edcae7517dc5057cab18945990c9c1440
hersusanto <herisusanto.id@gmail.com>
2019-09-09 22:13:26 +0700

    Build first app using swift & xcode

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Heri Susanto on 04/09/19.
+//  Copyright © 2019 Heri Susanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceImageSatu: UIImageView!
+    @IBOutlet weak var diceImageTiga: UIImageView!
+    
+    @IBOutlet weak var diceImageDua: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomDiceIndex1 = Int.random(in: 0...5)
+        randomDiceIndex2 = Int.random(in: 0...5)
+//        print (randomDiceIndex1)
+        
+        diceImageSatu.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageTiga.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    
+}
+
